4c65a6f4ccf2a29bee9e74caee2d5a208e21df4b
Artem Innokentiev <artinnok@mail.ru>
2016-10-25 18:58:32 +0300

    ADD CallbackView

--- moderator/api/views.py
+++ moderator/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.shortcuts import redirect
 from django.conf import settings
+import requests
+
+from core.models import Token
 
 
 class AuthorizeView(APIView):
@@ -14,5 +18,20 @@ class AuthorizeView(APIView):
 
 
 class CallbackView(APIView):
+    url = 'https://oauth.vk.com/access_token?client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&code={code}'
+
     def get(self, request, *args, **kwargs):
-        print(request.query_params)
+        code = request.query_params['code']
+        r = requests.get(self.url.format(
+            client_id=settings.CLIENT_ID,
+            client_secret=settings.CLIENT_SECRET,
+            redirect_uri=settings.REDIRECT_URI,
+            code=code
+        ))
+        json = r.json()
+        token, created = Token.objects.update_or_create(
+            access_token=json['access_token'],
+            defaults=json
+        )
+        return Response('ok')
+
