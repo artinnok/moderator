af6fa6ba0cb9adce3f5e526339fa60343e1068bf
Artem Innokentiev <artinnok@mail.ru>
2016-10-25 18:41:26 +0300

    ADD Token model

--- moderator/core/admin.py
+++ moderator/core/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
+from core.models import Token
+
+
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'access_token', 'expires_in')
+
+admin.site.register(Token, TokenAdmin)
+
 admin.site.unregister(User)
 admin.site.unregister(Group)
--- moderator/core/models.py
+++ moderator/core/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+from core import common_models as cm
+
+
+class Token(cm.Common):
+    """
+    Токен, полученный от Вконтакте
+    """
+    user_id = models.BigIntegerField(
+        verbose_name='ID пользователя'
+    )
+    access_token = models.CharField(
+        max_length=200,
+        verbose_name='Токен'
+    ),
+    expires_in = models.BigIntegerField(
+        verbose_name='Истекает в течение'
+    )
+
+    class Meta:
+        verbose_name = 'токен'
+        verbose_name_plural = 'токены'
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.user_id)
