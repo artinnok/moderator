83a791edf436a6fff4c7cfc9aa44d354928af43b
Artem Innokentiev <artinnok@mail.ru>
2016-10-27 11:02:55 +0300

    REFACTOR

--- moderator/api/fetch.py
+++ moderator/api/fetch.py
@@ -7,41 +7,40 @@ class Fetcher:
     owner_id = Club.objects.last().owner_id
     url = 'https://api.vk.com/method/{method_name}?{parameters}&access_token={access_token}&v=5.59'
 
-    def fetch(self, method, parameters):
+    def fetch(self, method, parameters, token):
         return requests.get(self.url.format(
             method_name=method,
             parameters=parameters,
-            access_token=Token.objects.last().access_token
+            access_token=token
         )).json()
 
-    def get_comment_list(self):
-        """пройдет по каждому id посту, и вытащит комменты"""
-        out = []
-        post_id_list = self.fetch_post_id_list()
-
-        for post_id in post_id_list:
-            out += (post_id, self.fetch_post_comment_list(post_id))
-        return out
-
-    def fetch_post_id_list(self):
+    def fetch_post_list(self, owner_id):
         """вернет id постов, которые имееют количество коментов > 0"""
         method = 'wall.get'
-        parameters = 'owner_id={owner_id}&count=10'.format(
-            owner_id=self.owner_id)
+        parameters = ('owner_id={owner_id}'
+                      '&count=10'.format(owner_id=owner_id))
+        token = Token.objects.last().access_token
 
-        json = self.fetch(method, parameters)
+        json = self.fetch(method, parameters, token)
         items = json['response']['items']
-        return (post['id'] for post in items if post['comments']['count'])
+        return items
 
-    def fetch_post_comment_list(self, post_id):
+    def filter_post_list(self, post_list):
+        return (post['id'] for post in post_list if post['comments']['count'])
+
+    def fetch_comment_list(self, owner_id, post_id):
         """вернет id коментов у которых меньше 5 лайков"""
         method = 'wall.getComments'
         parameters = ('owner_id={owner_id}&'
                       'post_id={post_id}&'
                       'need_likes=1&'
-                      'count=100'.format(owner_id=self.owner_id,
-                                         post_id=post_id))
+                      'count=100'.format(owner_id=owner_id, post_id=post_id))
+        token = Token.objects.last().access_token
 
-        json = self.fetch(method, parameters)
+        json = self.fetch(method, parameters, token)
         items = json['response']['items']
-        return [comment['id'] for comment in items if comment['likes']['count'] < 5]
+        return items
+
+    def filter_comment_list(self, comment_list):
+        return (comment['id'] for comment in comment_list
+                if comment['likes']['count'] < 5)
