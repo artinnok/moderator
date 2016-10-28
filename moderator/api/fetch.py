cd1225b558f717200133ce9c85b7a72424034355
Artem Innokentiev <artinnok@mail.ru>
2016-10-28 13:22:50 +0300

    REFACTOR

--- moderator/api/fetch.py
+++ moderator/api/fetch.py
@@ -1,50 +1,43 @@
 from celery import shared_task, chain
+from celery.contrib.methods import task
 import requests
 
 from core.models import Token
 
 
 class Fetcher:
-    url = 'https://api.vk.com/method/{method_name}?{parameters}&access_token={access_token}&v=5.59'
-
     def __init__(self, owner_id):
         self.owner_id = owner_id
 
-    def fetch_post_list(self, owner_id):
+    def fetch_post_list(self):
         method = 'wall.get'
         parameters = ('owner_id={owner_id}'
-                      '&count=10'.format(owner_id=owner_id))
+                      '&count=10'.format(owner_id=self.owner_id))
         token = Token.objects.last().access_token
 
-        json = fetch.s(method, parameters, token)
+        json = fetch
         return json
 
-    def filter_post_list(self, post_list):
-        return (post['id'] for post in post_list if post['comments']['count'])
-
-    def fetch_comment_list(self, owner_id, post_id):
+    def fetch_comment_list(self, post_id):
         method = 'wall.getComments'
         parameters = ('owner_id={owner_id}&'
                       'post_id={post_id}&'
                       'need_likes=1&'
-                      'count=100'.format(owner_id=owner_id, post_id=post_id))
+                      'count=100'.format(owner_id=self.owner_id,
+                                         post_id=post_id))
         token = Token.objects.last().access_token
 
-        json = fetch.s(method, parameters, token)
+        json = fetch
         return json
 
-    def filter_comment_list(self, comment_list):
-        return [comment['id'] for comment in comment_list
-                if comment['likes']['count'] < 5]
-
-    def delete_comment(self, owner_id, comment_id):
+    def delete_comment(self, comment_id):
         method = 'wall.deleteComment'
         parameters = ('owner_id={owner_id}&'
-                      'comment_id={comment_id}'.format(owner_id=owner_id,
+                      'comment_id={comment_id}'.format(owner_id=self.owner_id,
                                                        comment_id=comment_id))
         token = Token.objects.last().access_token
 
-        json = fetch.s(method, parameters, token)
+        json = fetch
         return json
 
 
@@ -52,21 +45,16 @@ class Fetcher:
 def start():
     out = []
     fetcher = Fetcher(-112088372)
-    post_list = chain(fetcher.fetch_post_list(fetcher.owner_id),
-                      fetcher.filter_post_list())
-    post_list = post_list.get()
-    for post in post_list:
-        comment_list = chain(fetcher.fetch_comment_list(fetcher.owner_id, post),
-                             fetcher.filter_comment_list())
-        comment_list = comment_list.get()
-        out += comment_list
-    for comment_id in out:
-        fetcher.delete_comment(fetcher.owner_id, comment_id)
+    fetcher.fetch_post_list(-112088372)
 
 
-@shared_task(rate_limit='3/s')
+@shared_task(name='fetch', rate_limit='3/s')
 def fetch(method, parameters, token):
-    url = 'https://api.vk.com/method/{method_name}?{parameters}&access_token={access_token}&v=5.59'
+    url = ('https://api.vk.com/method/'
+           '{method_name}?'
+           '{parameters}&'
+           'access_token={access_token}'
+           '&v=5.59')
     return requests.get(url.format(
         method_name=method,
         parameters=parameters,
@@ -74,3 +62,12 @@ def fetch(method, parameters, token):
     )).json()
 
 
+@shared_task(name='filter_post_list')
+def filter_post_list(post_list):
+    return (post['id'] for post in post_list if post['comments']['count'])
+
+
+@shared_task(name='filter_comment_list')
+def filter_comment_list(comment_list):
+    return (comment['id'] for comment in comment_list
+            if comment['likes']['count'] < 5)
