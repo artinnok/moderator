0646a3a6aa257a0d085ad9125304488b689a9d7d
Artem Innokentiev <artinnok@mail.ru>
2016-11-17 22:35:11 +0300

    ADD new login

--- moderator/static/src/blocks/iframe/iframe.js
+++ moderator/static/src/blocks/iframe/iframe.js
@@ -1,4 +1,19 @@
+var url = require("url");
+
 $(document).ready(function () {
+    var query = url.parse(window.location.href, true).query;
+    var owner_id = query.group_id;
+    var permissions = query.api_settings;
+    var access_token = query.access_token;
+    var installed = query.is_app_user;
     var mask = 262144;
-    VK.callMethod("showGroupSettingsBox", mask);
+
+    if (installed == false){
+        VK.callMethod("showGroupSettingsBox", mask);
+    }
+    else{
+        if (permissions != mask){
+            VK.callMethod("showGroupSettingsBox", mask);
+        }
+    }
 });
