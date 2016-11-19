d427b144991b6c0b8a33e9a86a43f3464eb7c0c4
Artem Innokentiev <artinnok@mail.ru>
2016-11-19 20:04:03 +0300

    ADD new iframe settings

--- moderator/static/src/blocks/iframe/iframe.js
+++ moderator/static/src/blocks/iframe/iframe.js
@@ -4,28 +4,25 @@ VK.init(function () {
     var query = url.parse(window.location.href, true).query;
     var owner_id = query.group_id;
     var permissions = query.api_settings;
-    var access_token = query.access_token;
     var installed = query.is_app_user;
-    var mask = 4096;
-    console.log(query);
+    var mask = 8192;
 
     if (installed == false){
-        VK.callMethod("showGroupSettingsBox", mask);
+        VK.callMethod("showSettingsBox", mask);
     }
     else{
-        if (permissions != mask){
-            VK.callMethod("showGroupSettingsBox", mask);
+        if (permissions < mask){
+            VK.callMethod("showSettingsBox", mask);
         }
     }
-    VK.addCallback("onGroupSettingsChanged", function(data){
-       console.log(data);
-    });
-    VK.api("wall.deleteComment", {
-        "owner_id": -132545756,
-        "comment_id": 7
-    }, function (data) {
-        console.log(data);
-    })
-}, function () {
 
+
+    VK.addCallback("onSettingsChanged", function(perm, token){
+        window.localStorage.setItem('moder.access_token', token);
+        window.localStorage.setItem('moder.owner_id', owner_id);
+    });
+}, function () {
+    window.location.reload()
 }, '5.60');
+
+
